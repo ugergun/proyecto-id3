0fd9fb83cfe7fece13605a992bf124a895277e3a
oscar7685 <oscar7685@590f8998-65e6-39af-5594-c2ebef75cf73>
2011-08-23 12:51:46 +0000

    id3 con jtree

---
+++ trunk/id3/src/id3/Condicion.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package id3;
+
+/**
+ *
+ * @author Luis Miguel
+ */
+public class Condicion {
+
+    private String atributo;
+    private String valor;
+
+    public Condicion(String atributo, String valor) {
+        this.atributo = atributo;
+        this.valor = valor;
+    }
+
+    public String getAtributo() {
+        return atributo;
+    }
+
+    public void setAtributo(String atributo) {
+        this.atributo = atributo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+
+        if ( ! ( other instanceof Condicion ) ) return false;
+
+        Condicion cond = (Condicion) other;
+
+        return this.atributo.equals( cond.atributo ) &&
+               this.valor.equals( cond.valor );
+
+    }
+
+
+
+}
